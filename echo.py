ce0341ff8a5556f81e27ff4b5fbc5f84e5784de7
Animeboiiiiii <86106325+Animeboiiiiii@users.noreply.github.com>
2022-01-04 16:17:58 +0530

    Update echo.py

--- echo.py
+++ echo.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 app = Client ('app', api_id = API_ID,api_hash=API_HASH,bot_token=BOT_TOKEN)
 
 @app.on_messege(filters_command('UwU')
-def ok(_,messege):
+def(_,messege):
 messege.reply_text("no UWU pay 69$ now")
 
 
