f2f198afbf0ea6632c707fbe2708d0604c10c425
Animeboiiiiii <86106325+Animeboiiiiii@users.noreply.github.com>
2022-01-04 11:05:09 +0530

    Create echo.py

---
+++ echo.py
@@ -0,0 +1,12 @@
+from pyrogram import Client, filters
+
+app = Client ('app', api_id = API_ID,api_hash=API_HASH,bot_token=BOT_TOKEN)
+
+@app.on_messege(filters_command('UwU')
+def ok(_,messege)
+messege.reply_text("no UWU pay 69$ now")
+
+
+app.run()
+
+
