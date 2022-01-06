f7843b6617ebb473a3c43bf516a3e053b1e31751
Animeboiiiiii <86106325+Animeboiiiiii@users.noreply.github.com>
2022-01-06 16:11:11 +0530

    Update echo.py

--- echo.py
+++ echo.py
@@ -7,7 +7,7 @@ API_HASH = os.environ.get('API_HASH')
 
 app = Client ('app', api_id = API_ID,api_hash=API_HASH,bot_token=TOKEN)
 
-@app.on_messege(filters_command('UwU'))
+@app.on_message(filters_command('UwU'))
 def echo(_,messege):
     messege.reply_text("no UWU pay 69$ now")
 
