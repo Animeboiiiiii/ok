0f666f85f634b0b2ea631a57ea8fe20ed1f92a1b
Kakarot <81678105+ItzKakarotto@users.noreply.github.com>
2022-01-07 11:50:25 +0530

    Update and rename echo.py to botto.py

---
+++ botto.py
@@ -0,0 +1,27 @@
+from pyrogram import Client, filters
+import os
+
+TOKEN = os.environ.get('TOKEN')
+API_ID = os.environ.get('API_ID')
+API_HASH = os.environ.get('API_HASH')
+
+app = Client ('app', api_id = API_ID,api_hash=API_HASH,bot_token=TOKEN)
+
+@app.on_message(filters.command('UwU'))
+def echo(_,messege):
+    messege.reply_text("no UWU pay 69$ now")
+
+@app.on_message(filters.command('info'))
+def info(_, message):
+    usr = message.user
+    caption = f"<b>User ID:<\b> {usr.id}"
+    caption += f"<b>Username:<\b> {usr.username}\n"
+    caption += f"<b>Is A Bot?:<\b> {usr.is_bot}\n"
+    caption += f"<b>Status:<\b> {usr.status}\n"
+    caption += f"<b>DC id:<\b> {usr.dc_id}\n"
+    message.reply_photo(photo=usr.photo, caption=caption)
+
+
+app.run()
+
+
